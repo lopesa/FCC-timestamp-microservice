5d51c9a18027ef3f3adeeda987b064e91292af8b
Tony Lopes <tony@lopesdesign.com>
2016-04-07 19:42:09 -0700

    working on heroku

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,10 @@ var Timestamper = require(process.cwd() + '/app/controllers/timestamper.server.j
 module.exports = function (app) {
    var timestamper = new Timestamper();
 
+   app.route('/')
+      .get(function (req, res) {
+         res.sendFile(process.cwd() + '/public/index.html');
+      });
+
    app.route('/*').get(timestamper.convert)
 };
--- server.js
+++ server.js
@@ -10,10 +10,24 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 // prettify json responses
 app.set('json spaces', 3);
 
+// from example heroku app
+app.set('port', (process.env.PORT || 5000));
+
 routes(app);
 
 
+// dev
+// app.listen(3000, function () {
+//    console.log('Node.js listening on port 3000...');
+// });
 
-app.listen(3000, function () {
-   console.log('Node.js listening on port 3000...');
+// prod
+// app.listen(80);
+// app.listen(80, function () {
+//    console.log('Node.js listening on port 80...');
+// });
+
+//from example heroku app
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
