3ac53fcf770922f272d57493bcab71c44140dd63
Tony Lopes <tony@lopesdesign.com>
2016-04-07 20:20:48 -0700

    initial commit

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 // prettify json responses
 app.set('json spaces', 3);
 
+// from example heroku app
+app.set('port', (process.env.PORT || 5000));
+
 routes(app);
 
 
@@ -19,7 +22,12 @@ routes(app);
 // });
 
 // prod
-app.listen(80);
+// app.listen(80);
 // app.listen(80, function () {
 //    console.log('Node.js listening on port 80...');
 // });
+
+//from example heroku app
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
