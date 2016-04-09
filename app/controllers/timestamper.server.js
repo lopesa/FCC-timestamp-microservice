45130da3def89368a6f2cea77227678724ae902c
Tony Lopes <tony@lopesdesign.com>
2016-04-08 20:03:42 -0700

    initial commit

--- app/controllers/timestamper.server.js
+++ app/controllers/timestamper.server.js
@@ -10,11 +10,25 @@ function timestamper () {
    this.convert = function (req, res) {
       // res.send(req.params[0])
       var inputDate = req.params[0],
+         myDate = new Date(inputDate).toString(),
+         UTCDate = new Date(inputDate).toUTCString(),
+         timeZoneOffsetSecs, 
          buildDates,
          finalDateObj;
 
+      
+      // console.log(myDate);
+      // console.log(UTCDate);
+
+      
       buildDates = function buildDates() {
-         var dates = {}
+         
+         var dates = {},
+            UTCdate,
+            UTCmonth,
+            UTCyear,
+            UTCstring;
+         
          // string case
          if (isNaN(inputDate)) {
             // catches non-date strings
@@ -23,8 +37,17 @@ function timestamper () {
                dates.linux = null;
             }
             else {
-               dates.natural = new Date(inputDate).toDateString();
-               dates.linux = new Date(inputDate).getTime();
+               timeZoneOffsetSecs = new Date(inputDate).getTimezoneOffset() * 60
+
+               dates.natural = new Date(inputDate).toLocaleString(
+                  'en-US', {
+                     // timeZone: 'UTC',
+                     month: 'long',
+                     day: 'numeric',
+                     year: 'numeric'
+                  }
+               );
+               dates.linux = new Date(inputDate) / 1000 - timeZoneOffsetSecs;
             }
          }
 
@@ -36,7 +59,15 @@ function timestamper () {
                dates.linux = null;
             }
             else {            
-               dates.natural = new Date(Number(inputDate)).toDateString();
+               
+               dates.natural = new Date(Number(inputDate) * 1000).toLocaleString(
+                  'en-US', {
+                     timeZone: 'UTC',
+                     month: 'long',
+                     day: 'numeric',
+                     year: 'numeric'
+                  }
+               );
                dates.linux = inputDate;
             }
          }
